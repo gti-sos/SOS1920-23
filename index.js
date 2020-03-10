6ac97fc5198a445875f3e4e9fc9177f62455bc4a
antoniocl11 <antcablin1@alum.us.es>
2020-03-10 13:18:54 +0000

    trabajando en el repositorio

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hola mundo")
+const express = require("express");
+
+var app = express();		//Servidor web
+var fecha = new Date();
+var hora_actual = fecha.getHours();
+var minu = fecha.getMinutes();
+
+app.get("/time",(request,response)=>{
+	response.send("<html>"+hora_actual+":"+minu+"</html>");
+});
+
+app.listen(80);
+	
+console.log("server ready");
