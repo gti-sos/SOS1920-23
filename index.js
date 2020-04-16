cf90c20f6be657b75b0329a2de12a27deab6082b
antoniocl11 <antcablin1@alum.us.es>
2020-04-16 09:11:21 +0000

    beta_test_v2.1

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express');	//Importacion modulo express
+const bodyParser = require('body-parser');	//Importacion modulo body-parser
+//const dataStore = require('nedb');	//Importacion base de datos nedb
+const path = require('path');	//Importacion mnodulo path
 
-var app = express();
-var port = process.env.PORT || 80;
+const app = express();	//constante app para la utilizacion de express
+const port = process.env.PORT || 80;	//constante port para la utilizacion del puerto 80 u otro.
+//const dbFileNameFires = path.join(__dirname , "fires-stats.db");	//constante ruta de archivos de base de datos
+
+//Llamada constantes de APIs
+const apiAntonio = require(path.join(__dirname , "apiAntonio"));	//Importación modulo API de Antonio
+apiAntonio(app);
+
+
+
+//--------------------------------------------------------------------------------
 
 app.use(bodyParser.json()); //Par cuando llegan datos transformarlos automรกticamente
 app.use('/', express.static('./public'));
@@ -11,6 +22,7 @@ app.get('/cool', (request, response) => {
 	response.send('<html>' + cool() + '</html>');
 });
 
+//Api Ejemplo contactos ------------------------------------------------------------
 var contacts = [
 	{
 		name: 'peter',
@@ -35,6 +47,8 @@ app.post(BASE_API_URL + '/contacts', (req, res) => {
 app.get('/cool', (request, response) => {
 	response.send('<html>' + cool() + '</html>');
 });
+
+//------------------------------------------------------------------------------------
 
 //API Alejandro
 var offworks_stats = [
@@ -343,187 +357,7 @@ app.post(BASE_API_URL + '/cigarretes-sales/:community', (req, res) => {
 });
 
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-//API Antonio
-
-var fires = [
-	{
-		community: 'andalucia',
-		year: 2007,
-		total_fire: 819,
-		forest_area: 6296.75,
-		non_forest_area: 3282.53
-	},
-	{
-		community: 'aragon',
-		year: 2007,
-		total_fire: 415,
-		forest_area: 1860.38,
-		non_forest_area: 611.51
-	}
-];
-//GET LOADINITIALDATA
-app.get(BASE_API_URL + '/fires-stats/loadInitialData', (req, res) => {
-	fires = [
-		{
-			community: 'andalucia',
-			year: 2007,
-			total_fire: 819,
-			forest_area: 6296.75,
-			non_forest_area: 3282.53
-		},
-		{
-			community: 'aragon',
-			year: 2007,
-			total_fire: 415,
-			forest_area: 1860.38,
-			non_forest_area: 611.51
-		}
-	];
-	if (fires.length >= 1) {
-		res.status(200).send('THERE IS ALREADY CREATED');
-	} else {
-		res.send(JSON.stringify(fires, null, 2));
-	}
-});
-//-----------------------------------------------------------------------------------------------
-
-//GET fires-stats
-app.get(BASE_API_URL + '/fires-stats', (req, res) => {
-	if (fires.length == 0) {
-		res.status(400).send('THERE IS NO DATA');
-	} else {
-		res.send(JSON.stringify(fires, null, 2));
-	}
-});
-//-----------------------------------------------------------------------------------------------
-
-//POST fires-stats
-
-app.post(BASE_API_URL + '/fires-stats', (req, res) => {
-	var newFire = req.body;
-
-	if (newFire == '' || newFire.community == null) {
-		res.sendStatus(400); //Codigo 400(BAD REQUEST)
-	} else {
-		fires.push(newFire);
-		res.sendStatus(201); //Codigo 201(CREATED)
-	}
-});
-//-----------------------------------------------------------------------------------------------
-
-//GET fires-stats/XXXX
-app.get(BASE_API_URL + '/fires-stats/:community', (req, res) => {
-	var community = req.params.community;
-
-	var filteredfires = fires.filter(f => {
-		//Filter va iterando y devolviendo todos los elementos del array, y comprueba que cumplen una determinada condicion
-		return f.community == community;
-	});
-	if (filteredfires.length >= 1) {
-		res.send(filteredfires[0]); //Devuelve el primer elemento del array
-	} else {
-		res.sendStatus(404); //FIRE-DATA NOT FOUND
-	}
-});
-//-----------------------------------------------------------------------------------------------
-
-//DELETE fires-stats/XXXX
-app.delete(BASE_API_URL + '/fires-stats/:community', (req, res) => {
-	var community = req.params.community;
-
-	var filteredfires = fires.filter(f => {
-		//Filter va iterando y devolviendo todos los elementos del array, y comprueba que los nombres de las comunidades son distintos
-		return f.community != community;
-	});
-	if (filteredfires.length < fires.length) {
-		fires = filteredfires;
-		res.sendStatus(200); //Codigo 200(OK), recurso eliminado correctamente
-	} else {
-		res.sendStatus(404); //FIRE DATA NOT FOUND
-	}
-});
-//-----------------------------------------------------------------------------------------------
-
-//PUT fires-stats/XXXX
-
-app.put(BASE_API_URL + '/fires-stats/:community', (req, res) => {
-	var community = req.params.community;
-	var body = req.body;
-	var filteredfires = fires.filter(c => {
-		return c.community == community;
-	});
-
-	if (filteredfires.length == 1) {
-		var updateData = fires.map(f => {
-			var upData = f;
-			if (f.community == community) {
-				for (var p in body) {
-					upData[p] = body[p];
-				}
-			}
-			return updateData;
-		});
-
-		fires.push = updateData;
-		res.sendStatus(200); //Recurso encontrado y modificado
-	} else {
-		res.sendStatus(404); //Recurso no encontrado
-	}
-});
-//-----------------------------------------------------------------------------------------------
-/*
-// PUT fires-stats/XXX/XXX
-app.put(BASE_API_URL + '/fires-stats/:community/:year', (req, res) => {
-	var community = req.params.community;
-	var year = req.params.year;
-
-	var data = req.body;
-
-	var filteredFires = fires_stats.filter(c => {
-		return c.community == community && c.year == year;
-	});
-
-	if (filteredFires.length == 0) {
-		res.sendStatus(404, 'NOT FOUND');
-	} else {
-		var filteredFires2 = fires_stats.filter(c => {
-			return c.community != community || c.year != year;
-		});
-
-		if (data == '' || data.community == null || data.year == null) {
-			res.sendStatus(400, 'BAD REQUEST');
-		} else {
-			Fires_stats = filteredFires2;
-			filteredFires2.push(data);
-			res.sendStatus(200, 'OK');
-		}
-	}
-});
-//-----------------------------------------------------------------------------------------------
-*/
-//POST FIRES/XXXX (Esto debe de dar un error de método no permitido)
-app.post(BASE_API_URL + '/fires-stats/:community', (req, res) => {
-	res.sendStatus(405); //Method not allowed
-});
-//-----------------------------------------------------------------------------------------------
-
-//PUT FIRES (Esto debe de dar un error de método no permitido)
-
-app.put(BASE_API_URL + '/fires-stats', (req, res) => {
-	res.sendStatus(405); //Method Not Allowed
-});
-//-----------------------------------------------------------------------------------------------
-
-//DELETE FIRES
-app.delete(BASE_API_URL + '/fires-stats', (req, res) => {
-	fires = []; //Creo un array vacio
-	res.sendStatus(200); //Envio el codigo de respuesta 200(OK) si se ha hecho correctamente el borrado del array.
-});
-
-//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 app.listen(port, () => {
 	console.log('server ready');
 });
