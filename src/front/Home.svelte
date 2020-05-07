f9fbe69467da7da2c74f7b984c49a79ae23b95ba
joserra123 <61206598+joserra123@users.noreply.github.com>
2020-05-07 19:04:09 +0200

    Update Home.svelte

--- src/front/Home.svelte
+++ src/front/Home.svelte
@@ -7,6 +7,6 @@
 	<div class="div-home">
 		<Button type="button" color="info" onclick="window.location.href='#/fires-stats'"> API de estadísticas de incendios en España</Button>
 		<Button type="button" color="info" onclick="window.location.href='#cigarretes-sales/'"> API de estadísticas de venta de tabaco en España</Button>
-		<Button type="button" color="info" onclick="window.location.href='#/'"> API 2 </Button>
+		<Button type="button" color="info" onclick="window.location.href='#offworks-stats/'"> API de accidentes en España </Button>
 	</div>
 </main>
