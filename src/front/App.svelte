f5f0e0c0770d328679342e24c6912d57f26fc972
Antonio Cabello Linares <51081995+antoniocl11@users.noreply.github.com>
2020-05-07 18:59:38 +0200

    Add files via upload

---
+++ src/front/App.svelte
@@ -0,0 +1,39 @@
+<script>
+
+	import CigarretesTable from './CigarretesAPI/CigarretesTable.svelte';
+	
+
+
+
+
+
+	import Router from 'svelte-spa-router';
+
+	import Home from './Home.svelte';
+	import NotFound from './NotFound.svelte';
+
+	//Import archivos API Fires
+	import FiresStatsTable from './Fires_Stats_API/FiresStatsTable.svelte';
+	import EditFiresStats from './Fires_Stats_API/EditFiresStats.svelte';
+
+
+	const routes = {
+		"/" : Home,
+		
+
+		"/fires-stats" : FiresStatsTable,
+		"/fires-stats/:community/:year" : EditFiresStats,
+
+		"*" : NotFound
+	};
+
+
+</script>
+
+
+<main>
+	
+	<Router {routes} />
+	
+</main>
+
