3e3bc45c17e6b864367f4d77d1db7f9c9883e5bb
joserra123 <jorampe18@gmail.com>
2020-05-28 12:23:09 +0200

    cambios joserra

--- src/front/Home.svelte
+++ src/front/Home.svelte
@@ -31,6 +31,18 @@
 						</DropdownMenu>
 						</Dropdown>
 </td>
+
+<th>
+					<Dropdown {isOpen} toggle={() => (isOpen = !isOpen)}>
+						<DropdownToggle outline caret>Gráficas de venta de paquetes de tabaco en España</DropdownToggle>
+						<DropdownMenu>
+							<DropdownItem href="#/graph-cigarretes-sales">HighChart</DropdownItem>
+							<DropdownItem href="#/chart-cigarretes-sales">Chart.js</DropdownItem>
+
+
+						</DropdownMenu>
+						</Dropdown>
+</th>
 <p><Button type="button" color="white" style="color:rgb(40, 83, 53);border-color:brown;margin-top: 5%;" onclick="window.location.href='#/graph-offworks-stats'"> Grafica HighChart de bajas laborales en España </Button></p>
 <p><Button type="button" color="white" style="color:rgb(40, 83, 53);border-color:brown;" onclick="window.location.href='#/Rgraph-offworks-stats'"> Grafica Rgraph de bajas laborales en España </Button></p>
 
@@ -41,6 +53,8 @@
 
 <p><Button type="button" color="info" onclick="window.location.href='#/fires-stats-integrations/integrations'"> Integraciones Fires Stats </Button></p>
 <p><Button type="button" color="info" onclick="window.location.href='#/offworks-stats-integrations/integrations'"> Integraciones Offworks Stats </Button></p>
+<p><Button type="button" color="info" onclick="window.location.href='#/cigarretes-sales-integrations/integrations'"> Integraciones cigarretes-sales </Button></p>
+
 
 <h4>Gráfica Grupal:</h4>
 
