b3cde711ecc0ed3e2a2a29b1d386b703f509cfc2
alegrandoi <61064395+alegrandoi@users.noreply.github.com>
2020-06-03 20:19:05 +0200

    arreglo video

--- src/front/About.svelte
+++ src/front/About.svelte
@@ -14,16 +14,9 @@
 
   <h4>Videos explicativos:</h4>
 <Button color="info" on:click="{pop}">INCENDIOS FORESTALES</Button>
-<div class="card">
-  <div class="embed-responsive embed-responsive-16by9 video">
-      <iframe title="cig" width="560" height="315" src="https://www.youtube.com/embed/HhXnWtQvIHg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-  </div>
-<div class="card-body">
-  <h5 class="card-title">Ventas de paquete de tabaco</h5>
-  <p class="card-text">Vídeo explicativo sobre la API venta de paquetes de tabaco en España.</p>
-</div>
-</div>
-<h5>Videos explicativo offworkApi:</h5>
+<h5>Video explicativo cigarretteApi:</h5>
+<iframe title="cig" width="560" height="315" src="https://www.youtube.com/embed/HhXnWtQvIHg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<h5>Video explicativo offworkApi:</h5>
 <iframe title="off" width="560" height="315" src="https://www.youtube.com/embed/qKW2lMeceCs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   	
 
