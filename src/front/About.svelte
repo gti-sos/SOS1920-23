204eeae5a2781a25d7d1c80f6a51ece655aab379
joserra123 <jorampe18@gmail.com>
2020-06-03 20:04:04 +0200

    video joserra

--- src/front/About.svelte
+++ src/front/About.svelte
@@ -11,9 +11,18 @@
   let isOpen = false;
 </script>
 <body style="text-align:center;">
-<h4>Videos explicativos:</h4>
+
+  <h4>Videos explicativos:</h4>
 <Button color="info" on:click="{pop}">INCENDIOS FORESTALES</Button>
-<Button color="info" on:click="{pop}">VENTAS DE TABACO</Button>
+<div class="card">
+  <div class="embed-responsive embed-responsive-16by9 video">
+      <iframe width="900" height="506" src="https://www.youtube.com/watch?v=HhXnWtQvIHg&feature=youtu.be" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </div>
+<div class="card-body">
+  <h5 class="card-title">Ventas de paquete de tabaco</h5>
+  <p class="card-text">Vídeo explicativo sobre la API venta de paquetes de tabaco en España.</p>
+</div>
+</div>
 <Button color="info" on:click="{pop}">BAJAS LABORALES</Button>
   	
 
