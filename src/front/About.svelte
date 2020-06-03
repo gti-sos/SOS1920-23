75e25fc0642d5f6fe48be5c1ee33bb8fc356d445
Antonio Cabello Linares <51081995+antoniocl11@users.noreply.github.com>
2020-06-03 21:08:21 +0200

    Update About.svelte

--- src/front/About.svelte
+++ src/front/About.svelte
@@ -1,19 +1,12 @@
 <script>
     import {pop} from "svelte-spa-router";
     import Button from "sveltestrap/src/Button.svelte";
-    import Table from "sveltestrap/src/Table.svelte";
-	import {
-    Dropdown,
-    DropdownItem,
-    DropdownMenu,
-    DropdownToggle
-  } from 'sveltestrap';
-  let isOpen = false;
+	
 </script>
 <body style="text-align:center;">
 
   <h4>Videos explicativos:</h4>
-<Button color="info" on:click="{pop}">INCENDIOS FORESTALES</Button>
+
 <h5>Video explicativo cigarretteApi:</h5>
 <iframe title="cig" width="560" height="315" src="https://www.youtube.com/embed/HhXnWtQvIHg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 <h5>Video explicativo offworkApi:</h5>
@@ -22,5 +15,5 @@
 <iframe width="560" height="315" src="https://www.youtube.com/embed/ETMqhWTBeF4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   	
 
-<p><Button outline color="secondary" on:click="{pop}">Volver</Button></p>
+<p><Button outline color="secondary" on:click="{pop}">Volver &#x21a9;</Button></p>
 </body>
