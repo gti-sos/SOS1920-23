3ebfcacdfe279d518b2f0799b4443caa6bc61072
alegrandoi <61064395+alegrandoi@users.noreply.github.com>
2020-06-03 20:11:48 +0200

    video

--- src/front/About.svelte
+++ src/front/About.svelte
@@ -16,14 +16,15 @@
 <Button color="info" on:click="{pop}">INCENDIOS FORESTALES</Button>
 <div class="card">
   <div class="embed-responsive embed-responsive-16by9 video">
-      <iframe width="900" height="506" src="https://www.youtube.com/watch?v=HhXnWtQvIHg&feature=youtu.be" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe title="cig" width="900" height="506" src="https://www.youtube.com/watch?v=HhXnWtQvIHg&feature=youtu.be" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
 <div class="card-body">
   <h5 class="card-title">Ventas de paquete de tabaco</h5>
   <p class="card-text">Vídeo explicativo sobre la API venta de paquetes de tabaco en España.</p>
 </div>
 </div>
-<Button color="info" on:click="{pop}">BAJAS LABORALES</Button>
+<h5>Videos explicativo offworkApi:</h5>
+<iframe title="off" width="560" height="315" src="https://www.youtube.com/embed/qKW2lMeceCs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   	
 
 <p><Button outline color="secondary" on:click="{pop}">Volver</Button></p>
